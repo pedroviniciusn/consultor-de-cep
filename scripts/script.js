2c8821065022ab6e701f275ce51aefd897a8a826
Pedro Vinicius <pedro.vinicius510@gmail.com>
2022-04-01 23:21:29 -0300

    corrigindo bugs

--- scripts/script.js
+++ scripts/script.js
@@ -16,7 +16,8 @@ formulario.onsubmit = event => {
         inputCep.classList.remove('hidden')
         
         async function preencherDados () {
-            const url = `http://viacep.com.br/ws/${inputCep.value}/json/`
+            
+            const url = `https://viacep.com.br/ws/${inputCep.value}/json/`
             await fetch(url)
             .then(response => response.json())
             .then(data => {
